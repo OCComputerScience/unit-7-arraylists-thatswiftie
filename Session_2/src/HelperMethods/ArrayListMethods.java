c573ce7332c05b87f8d287cddf94fadd9138d1f1
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2024-12-19 13:38:47 -0500

    Done with ArrayListMethods. 2/3 done

--- Session_2/src/HelperMethods/ArrayListMethods.java
+++ Session_2/src/HelperMethods/ArrayListMethods.java
@@ -1,4 +1,49 @@
 package HelperMethods;
 
-public class ArrayListMethods {
+import java.util.ArrayList;
+
+public class ArrayListMethods
+{
+    public static void print(ArrayList<Double> arr)
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            System.out.println(arr.get(i));
+        }
+    }
+    public static ArrayList<Double> condense(ArrayList<Double> arr)
+    {
+        Double previousNum = 1.0;
+        Double newNum = 1.0;
+        ArrayList<Double> newArr = new ArrayList<>();
+        boolean firstTime = false;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            firstTime = !firstTime;
+            if(firstTime)
+            {
+                previousNum = arr.get(i);
+            }
+            else
+            {
+                newNum = arr.get(i) * previousNum;
+                newArr.add(newNum);
+            }
+        }
+        return newArr;
+    }
+    public static ArrayList<Double> duplicate(ArrayList<Double> arr)
+    {
+        ArrayList<Double> newArr = new ArrayList<>();
+        Double numberAdd = 1.0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                numberAdd = arr.get(i);
+                newArr.add(numberAdd);
+            }
+        }
+        return newArr;
+    }
 }
--- Session_2/src/HelperMethods/Main.java
+++ Session_2/src/HelperMethods/Main.java
@@ -1,4 +1,22 @@
 package HelperMethods;
 
-public class Main {
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Double> prices = new ArrayList<>();
+        prices.add(13.45);
+        prices.add(8.99);
+        prices.add(2.95);
+        prices.add(7.88);
+        prices.add(12.56);
+        prices.add(1.23);
+
+        ArrayListMethods.print(prices);
+        System.out.println(ArrayListMethods.condense(prices));
+        System.out.println(ArrayListMethods.duplicate(prices));
+
+    }
 }
