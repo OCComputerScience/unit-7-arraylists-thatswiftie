acc75cbe2b6f9ecdba11c2adc90c7343359e4634
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2024-12-19 13:12:25 -0500

    Finished the Traversing Odds! 1/3 done

--- Session_2/src/TraversingOdds/Main.java
+++ Session_2/src/TraversingOdds/Main.java
@@ -1,6 +1,30 @@
 package TraversingOdds;
 
+import java.util.ArrayList;
+
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numArr = new ArrayList<>();
+        for(int i = 1; i <= 50; i++)
+        {
+            numArr.add(i);
+        }
+        removeEvens(numArr);
+        for(int i = 0; i < numArr.size(); i++)
+        {
+            System.out.print(numArr.get(i) + " ");
+        }
+    }
+    public static void removeEvens(ArrayList<Integer> array)
+    {
+        for(int i = 0; i < array.size(); i++)
+        {
+            if(array.get(i) % 2 == 0)
+            {
+                array.remove(i);
+            }
+        }
+    }
 }
