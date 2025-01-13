fdc146499c4132c50336a53926b97bd771d02ef5
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2025-01-13 10:47:17 -0500

    Search done

--- Session_4/src/LinearSearch/Main.java
+++ Session_4/src/LinearSearch/Main.java
@@ -13,6 +13,20 @@ public class Main
         array.add(2.9);
         array.add(7.0);
 
+        System.out.println(search(array, 2.9));
 
+    }
+
+    public static int search(ArrayList<Double> nums, Double element)
+    {
+        if(nums.isEmpty()) return -1;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums.get(i).equals(element))
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
