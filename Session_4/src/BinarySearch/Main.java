2aa99c868a328f5abde3ed641ff1279a19c9ed2e
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2025-01-15 08:49:24 -0500

    Made code better

--- Session_4/src/BinarySearch/Main.java
+++ Session_4/src/BinarySearch/Main.java
@@ -24,7 +24,7 @@ public class Main
     {
         int left = 0;
         int right = intList.size();
-        while(left +1 != right)
+        while(left <= right)
         {
             int mid = left + (right-left) / 2;
             if(intList.get(mid) == target)
@@ -33,11 +33,11 @@ public class Main
             }
             else if (intList.get(mid) > target)
             {
-                right = mid;
+                right = mid - 1;
             }
             else
             {
-                left = mid;
+                left = mid + 1;
             }
         }
         return -1;
