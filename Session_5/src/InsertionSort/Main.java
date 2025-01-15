b7ee8670db4ae25bdc1fe1c961165565b6e3ed2e
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2025-01-15 11:40:38 -0500

    Insertion sort done...?

--- Session_5/src/InsertionSort/Main.java
+++ Session_5/src/InsertionSort/Main.java
@@ -15,13 +15,37 @@ public class Main
         System.out.println();
 
         // sort first array
-
+        insertionSort(array1);
         // sort second array
-
+        insertionSort(array2);
         System.out.print("First array sorted: ");
         System.out.println(Arrays.toString(array1));
         System.out.print("Second array sorted: ");
         System.out.println(Arrays.toString(array2));
+    }
+
+    public static void insertionSort(int[] arr)
+    {
+        for(int i = 1; i < arr.length; i++)
+        {
+            int currNumber =  arr[i];
+            int currIndex = i - 1;
+
+            while(arr[currIndex] > currNumber &&(currIndex >= 0))
+            {
+                arr[currIndex + 1] = arr[currIndex];
+                currIndex--;
+
+                //had to add this because it would throw an error at me without it (Something with currIndex >= 0 index out of bounds)
+                if(currIndex < 0)
+                {
+                    break;
+                }
+            }
+
+            arr[currIndex + 1] = currNumber;
+            System.out.println(Arrays.toString(arr));
+        }
     }
 
 }
