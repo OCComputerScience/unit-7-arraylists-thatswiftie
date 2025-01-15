b817db01a3621271468aad719c06985ab5f81007
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2025-01-15 10:43:02 -0500

    Selection Sort done

--- Session_5/src/SelectionSort/Main.java
+++ Session_5/src/SelectionSort/Main.java
@@ -16,13 +16,40 @@ public class Main
         System.out.println();
 
         // sort first array
-
+        selectionSort(array1);
+        selectionSort(array2);
         // sort second array
 
         System.out.print("First array sorted: ");
         System.out.println(Arrays.toString(array1));
         System.out.print("Second array sorted: ");
         System.out.println(Arrays.toString(array2));
+    }
+
+    public static int[] selectionSort(int[] arr)
+    {
+        int swapCount = 0;
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            int currentMinIndex = i;
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[currentMinIndex] > arr[j])
+                {
+                    currentMinIndex = j;
+                }
+            }
+            if(currentMinIndex != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[currentMinIndex];
+                arr[currentMinIndex] = temp;
+                swapCount++;
+            }
+
+        }
+        System.out.println("Times Swapped: " + swapCount);
+        return arr;
     }
 
 }
